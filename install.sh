5484550d1d475c1bd21328411eae15a52dec15d4
Caio Mathielo <mathielo@gmail.com>
2019-03-05 12:53:01 +0100

    Add extra git configs and aliases to the repo setup

--- install.sh
+++ install.sh
@@ -12,6 +12,14 @@ echo 'source ~/.vim/.vimrc' > ~/.vimrc
 
 # Setting git's core editor
 git config --global core.editor "vim"
+git config --global commit.gpgsign true
+
+# List commits involved in a merge. https://stackoverflow.com/a/23508223/2752041
+git config --global alias.merge-log '!f() { git log --stat "$1^..$1"; }; f'
+
+# List commits of a merge, excluding the merge commit, in reverse order.
+# Usage: git merge-log-hashes CoMmItHaSh | xargs git cherry-pick
+git config --global alias.merge-log-hashes '!f() { git log --pretty=format:"%h" "$1^..$1" | grep -v $1 | tac; }; f'
 
 # Set Zsh DOTDIR
 echo 'ZDOTDIR=$HOME/.vim/.zsh' > ~/.zshenv
